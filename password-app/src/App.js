484335f8873f5c3d1cc71b56594adeb8ac7bdcbc
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-06-28 12:03:14 +0200

    ++ finished w bug

--- password-app/src/App.js
+++ password-app/src/App.js
@@ -1,9 +1,169 @@
-import "./app.module.css";
+import React, { useState, useEffect } from "react";
+import classes from "./app.module.css";
+
+import github from "./imgs/25231.png";
 
 function App() {
+  const [amount, setAmount] = useState("10");
+  const [numbers, setNumbers] = useState(true);
+  const [letters, setLetters] = useState(true);
+  const [symbols, setSymbols] = useState(true);
+  const [password, setPassword] = useState();
+  const [clicked, setClicked] = useState(false);
+
+  let disableNumbers = false;
+  let disableLetters = false;
+  let disableSymbols = false;
+
+  const lettersArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const symbolsArr = ["/", "*", "@", "#", "%", "$", ".", "&", "@"];
+  const numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  let filteredArray = [];
+  let displayArray = [];
+
+  useEffect(() => {
+    if (numbers && symbols && letters) {
+      filteredArray = lettersArr.concat(symbolsArr).concat(numbersArr);
+      disableLetters = false;
+      disableNumbers = false;
+      disableSymbols = false;
+    } else if (numbers === true && symbols === true) {
+      filteredArray = symbolsArr.concat(numbersArr);
+      disableNumbers = true;
+      disableSymbols = true;
+    } else if (symbols && letters) {
+      filteredArray = lettersArr.concat(symbolsArr);
+      disableLetters = true;
+      disableSymbols = true;
+    } else if (numbers && letters) {
+      filteredArray = lettersArr.concat(numbersArr);
+      disableNumbers = true;
+      disableLetters = true;
+    }
+
+    displayArray = [];
+
+    for (let i = 0; i < amount; i++) {
+      const randomIndex = Math.floor(Math.random() * filteredArray.length);
+      displayArray.push(filteredArray[randomIndex]);
+    }
+
+    const randomIndex = Math.floor(Math.random() * displayArray.length);
+    displayArray[randomIndex] = displayArray[randomIndex].toUpperCase();
+
+    setPassword(displayArray.join(""));
+  }, [amount, clicked]);
+
+  console.log(displayArray, password);
+
   return (
-    <div className="App">
-      <div className="wrapper"></div>
+    <div className={classes.app}>
+      <div className={classes.wrapper}>
+        <section className={classes.password}>
+          <input
+            disabled={true}
+            className={classes["display-pass"]}
+            value={password || ""}
+          ></input>
+          <div className={classes.btnwrapper}>
+            <button className={classes.copy}>Copy</button>
+            <button
+              className={classes.regenerate}
+              onClick={() => {
+                setClicked(!clicked);
+              }}
+            >
+              Generate
+            </button>
+          </div>
+        </section>
+        <section className={classes.settings}>
+          <div className={classes["input-wrapper-range"]}>
+            <label htmlFor="range">Length {`(${amount})`}</label>
+            <input
+              type="range"
+              id="range"
+              min="8"
+              max="20"
+              value={amount}
+              onChange={(e) => {
+                setAmount(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className={classes["input-wrapper"]}>
+            <input
+              disabled={disableNumbers}
+              type="checkbox"
+              id="numbers"
+              defaultChecked={numbers}
+              onChange={(e) => {
+                setNumbers(e.target.checked);
+              }}
+            ></input>
+            <label htmlFor="numbers">Numbers</label>
+          </div>
+          <div className={classes["input-wrapper"]}>
+            <input
+              disabled={disableLetters}
+              type="checkbox"
+              id="letters"
+              defaultChecked={letters}
+              onChange={(e) => {
+                setLetters(e.target.checked);
+              }}
+            ></input>
+            <label htmlFor="letters">Letters</label>
+          </div>
+          <div className={classes["input-wrapper"]}>
+            <input
+              disabled={disableSymbols}
+              type="checkbox"
+              id="special-char"
+              defaultChecked={symbols}
+              onChange={(e) => {
+                setSymbols(e.target.checked);
+              }}
+            ></input>
+            <label htmlFor="special-char">Symbols</label>
+          </div>
+          <footer>
+            <img src={github}></img>
+            <a href="https://github.com/Eliswer/password-generator">
+              Check the code on GitHub
+            </a>
+          </footer>
+        </section>
+      </div>
     </div>
   );
 }
